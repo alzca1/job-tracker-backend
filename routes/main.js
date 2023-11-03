94df1a287dbbd1ca46d3b3945ed44d3cb4f5a303
Álvaro Zájara <alzca1@gmail.com>
2023-11-03 19:16:56 +0100

    added single application get logic

--- routes/main.js
+++ routes/main.js
@@ -7,13 +7,20 @@ const createApplication = require("../controllers/application/create");
 const deleteApplication = require("../controllers/application/delete");
 const authenticationMiddleware = require("../middleware/auth");
 const getAllApplications = require("../controllers/application/getAll");
+const updateApplication = require("../controllers/application/update");
+const getApplication = require("../controllers/application/get");
 
+//## LOGIN ROUTES ## //
 router.route("/register").post(register);
 router.route("/login").post(login);
+
+//## APPLICATION ROUTES ## //
 router.route("/application/create").post(authenticationMiddleware, createApplication);
 router
   .route("/application/delete/:applicationId")
   .delete(authenticationMiddleware, deleteApplication);
 router.route("/application/getAll").get(authenticationMiddleware, getAllApplications);
+router.route("/application/:applicationId").get(authenticationMiddleware, getApplication);
+router.route("/application/update").put(authenticationMiddleware, updateApplication);
 
 module.exports = router;
